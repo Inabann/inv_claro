6b119b3e7741ef204adfe308ea07a0d04f092d24
inabann <25hbaam@gmail.com>
2017-02-23 14:00:12 -0500

    final final

--- models/login.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var loginSchema = new Schema({
-	username: String,
-	password: String
-});
-
-var model = mongoose.model('logins', loginSchema);
-module.exports = model;
